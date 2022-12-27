20c887cbbae34d521448c18cadf550711be5ac94
SanghoonSon <mue0329@naver.com>
2022-12-27 22:21:36 +0900

    feat: 메인 페이지 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,12 @@ dependencies {
     // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // thymeleaf
+    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.1.0")
 
     // db
     runtimeOnly("com.h2database:h2")
